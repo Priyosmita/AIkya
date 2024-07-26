85d8bea71eb1dcfd341858c960e3faf8fb393432
Priyosmita <daspriyosmita2003@gmail.com>
2024-07-26 21:06:04 +0530

    header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <header
-      className='bg-white w-full h-20'
+      className='bg-white w-full flex flex-row justify-between'
     >
+      <div className='flex items-center gap-7'>
+        <Image
+          src='/assets/logo2.png'
+          alt='AIkya logo'
+          width={62}
+          height={80}
+          className="cursor-pointer transform duration-300 hover:scale-110 mt-2 mb-2 ml-5"
+        />
+        {/* span is an alternative to div. Unlike div span only takes the place where it is located */}
+        <span className='cursor-default text-black text-2xl font-semibold transform transition duration-300 hover:scale-110'>
+          AIkya
+        </span>
+      </div>
+
+      <div className='flex justify-end'>
+        {/* all options are kept under nav for SEO purposes */}
+        <nav className='flex items-center space-x-6'>
+          <Link href="/" legacyBehavior>
+            <h2 className='cursor-pointer text-black text-2xl font-semibold transform transition duration-300 hover:scale-110'>About</h2>
+          </Link>
+          <nav className='flex items-center space-x-6'>
+          <Link href="/" legacyBehavior>
+            <h2 className='cursor-pointer text-black text-2xl font-semibold transform transition duration-300 hover:scale-110 mr-7'>Pro</h2>
+          </Link>
+        </nav>
+        </nav>
+      </div>
     </header>
   )
 }
