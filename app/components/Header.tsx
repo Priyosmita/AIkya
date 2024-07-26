0958fd32e72f7380a9c68df94f39643e4ccd5789
Priyosmita <daspriyosmita2003@gmail.com>
2024-07-26 20:26:50 +0530

    second

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,9 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
     <header
-      className='bg-white w-full py-2 px-8 flex justify-between items-center z-10 fixed transition-transform duration-300'
+      className='bg-white w-full h-20'
     >
-
     </header>
   )
 }
