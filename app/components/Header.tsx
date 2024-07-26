e54107e9f553ca072b96451cfd1b0e3641cd65f5
Priyosmita <daspriyosmita2003@gmail.com>
2024-07-26 20:21:06 +0530

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header
+      className='bg-white w-full py-2 px-8 flex justify-between items-center z-10 fixed transition-transform duration-300'
+    >
+
+    </header>
+  )
+}
+
+export default Header
