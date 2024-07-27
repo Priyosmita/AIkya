d30ec4473cd6574e1eea9959f418439e825746f5
Priyosmita <daspriyosmita2003@gmail.com>
2024-07-27 13:51:39 +0530

    fifth

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -1,6 +1,8 @@
+'use client'
 import React from 'react';
 import "../globals.css";
 import Image from 'next/image';
+import { SignIn, SignInButton, SignUpButton } from '@clerk/nextjs';
 
 const LandingPage = () => {
   return (
@@ -18,8 +20,8 @@ const LandingPage = () => {
         </div>
       </div>
       <div className="flex justify-center gap-x-101 mt-28 p-20">
-        <button type="button" className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-5xl px-5 py-2.5 text-center me-2 mb-2 transform trasition duration-300 hover:scale-110">Pink</button>
-        <button type="button" className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-5xl px-5 py-2.5 text-center me-2 mb-2 transform trasition duration-300 hover:scale-110">Pink</button>
+        <button type="button" className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-5xl px-5 py-2.5 text-center me-2 mb-2 transform trasition duration-300 hover:scale-110">Sign Up</button>
+        <button type="button" className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-5xl px-5 py-2.5 text-center me-2 mb-2 transform trasition duration-300 hover:scale-110">Sign In</button>
       </div>
     </div>
   );
