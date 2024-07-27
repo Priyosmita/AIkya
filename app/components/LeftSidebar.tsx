f8b2f98ba05b17ac04416c30c0cc7ebbe8800cf4
Priyosmita <daspriyosmita2003@gmail.com>
2024-07-28 01:36:44 +0530

    left sidebar

--- app/components/LeftSidebar.tsx
+++ app/components/LeftSidebar.tsx
@@ -6,7 +6,8 @@ import Link from 'next/link'
 const LeftSidebar = () => {
   return (
     <div className='w-80 bg-white p-4 rounded-2xl h-101 opacity-50'>
-          <nav className='flex flex-col space-y-4 text-black'>
+          <nav className='flex flex-col space-y-4 text-[#7ebaba] hover:text-[#f8b891] tranform transition duration-300 hover:scale-110'>
+            
           </nav>
         </div>
   )
