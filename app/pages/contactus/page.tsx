3f44ed0fcd299bbb4c18066dfb7fa6d83effe7b8
Priyosmita <daspriyosmita2003@gmail.com>
2024-08-02 00:59:55 +0530

    contact us

--- app/pages/contactus/page.tsx
+++ app/pages/contactus/page.tsx
@@ -8,15 +8,15 @@ const page = () => {
   return (
     <>
       <Header />
-      <div className='min-h-screen bgGradient flex flex-col items-center mt-10 pb-10'>
-        <div className='max-w-4xl mt-20 p-8 bg-white bg-opacity-50 rounded-2xl shadow-md'>
-          <h1 className='text-5xl text-[#7ebaba] font-semibold text-center mb-8'>Contact Us</h1>
-          <p className='text-2xl text-black text-center mb-8'>
+      <div className='min-h-screen bgGradient flex flex-col items-center mt-10 pb-14'>
+        <div className='max-w-4xl mt-20 p-8 bg-white bg-opacity-0 rounded-2xl '>
+          <h1 className='text-6xl text-white text-shadow-md font-semibold text-center mb-8 cursor-default'>Contact Us</h1>
+          <p className='text-2xl text-[#659595] text-center mb-8 cursor-default'>
             We would love to hear from you! Whether you have a question about features, pricing, or anything else, our team is ready to answer all your questions.
           </p>
           <form className='space-y-6'>
             <div>
-              <label htmlFor='name' className='block text-2xl font-medium text-black'>
+              <label htmlFor='name' className='block text-2xl font-medium text-[#659595] cursor-default'>
                 Name
               </label>
               <input
@@ -28,7 +28,7 @@ const page = () => {
               />
             </div>
             <div>
-              <label htmlFor='email' className='block text-2xl font-medium text-black'>
+              <label htmlFor='email' className='block text-2xl font-medium text-[#659595] cursor-default'>
                 Email
               </label>
               <input
@@ -40,7 +40,7 @@ const page = () => {
               />
             </div>
             <div>
-              <label htmlFor='message' className='block text-2xl font-medium text-black'>
+              <label htmlFor='message' className='block text-2xl font-medium text-[#659595] cursor-default'>
                 Message
               </label>
               <textarea
@@ -54,7 +54,7 @@ const page = () => {
             <div className='flex justify-center'>
               <button
                 type='submit'
-                className='text-black bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-3xl px-6 py-3 text-center transform transition duration-300 hover:scale-110'
+                className='text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-3xl px-6 py-3 text-center transform transition duration-300 hover:scale-110'
               >
                 Send Message
               </button>
