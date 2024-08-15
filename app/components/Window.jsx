579dbcc05b6a17bc993e982c61d380832d96c5ef
Priyosmita <daspriyosmita2003@gmail.com>
2024-08-15 23:05:10 +0530

    alignment

--- app/components/Window.jsx
+++ app/components/Window.jsx
@@ -17,13 +17,15 @@ const Window = ({ isMarketAnalysis, isSocialMedia, isSmartMatch, isFollowers, is
     <div className='WindowWidth bg-white rounded-2xl h-101 bg-opacity-50 mr-4 mt-24 p-4 '>
       {isMarketAnalysis && <MarketAnalysis />}
       <div className='flex justify-center'>
-      {isSocialMedia && <SocialMedia />}</div>
+        {isSocialMedia && <SocialMedia />}</div>
       {isSmartMatch && <SmartMatch />}
       {isFollowers && <Followers />}
       {isFollowing && <Following />}
       {isChats && <Chats />}
-      {isDonations && <Donations />}
-      {isFunding && <Funding />}
+      <div className='flex justify-center'>
+        {isDonations && <Donations />}
+        {isFunding && <Funding />}
+      </div>
       {isSearch && <Search />}
     </div>
   );
