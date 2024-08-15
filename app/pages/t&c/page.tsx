1daefae1b1866461a869f6ca488f2247658f0166
Priyosmita <daspriyosmita2003@gmail.com>
2024-08-15 12:38:28 +0530

    t&c

--- app/pages/t&c/page.tsx
+++ app/pages/t&c/page.tsx
@@ -3,15 +3,18 @@
 import React from 'react'
 import Header from '@/app/components/Header';
 import Footer from '@/app/components/Footer';
-import './globals.css';
+import '../../globals.css';
 
 const page = () => {
-  return (
-    <>
-    <Header/>
-    <Footer/>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <div className='min-h-screen bgGradient flex flex-col items-center mt-16 pt-9 pb-10'>
+                <div className='flex justify-center text-8xl text-white pt-12 text-shadow-md cursor-default'>Terms and Conditions</div>
+            </div>
+            <Footer />
+        </>
+    )
 }
 
 export default page
