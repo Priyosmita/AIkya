05e2c7b0480a3215a5356e0c6b6480ff8a2bda86
Priyos <156951756+Priyosmita@users.noreply.github.com>
2024-12-15 15:21:15 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GradientButton from '@/components/GradientButton';
 import Carousel3D from '@/components/Carousel3D';
+import { AuroraBackground } from '@/components/aurora-background';
 
 const Page = () => {
 
@@ -36,13 +37,14 @@ const Page = () => {
     <>
       {/* Parent container is relative */}
       <div className='h-screen relative'>
+      <AuroraBackground className='h-screen'/>
         {/* Child container is absolute */}
         <img src='/assets/hand.png' className='absolute bottom-0 left-0 w-full -top-8' alt='Hand' />
         <div className='absolute inset-0 flex flex-col text-white'>
           <div className='text-9xl flex justify-center pt-36'>AIkya</div>
           <div className='text-xl mt-5 flex justify-center'>Where Ideas Create Impact</div>
           <div className='flex justify-center pt-96 mt-16'>
-            <GradientButton gradientColors={["#ff99a7", "#9bd4c3"]} className=''>
+            <GradientButton gradientColors={["#ff99a7", "#9bd4c3"]} className='absolute'>
               <p className='text-center text-2xl'>Dashboard</p>
             </GradientButton>
           </div>
