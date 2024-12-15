4513dfe5822a803df5121fc2ad1339e76659ed72
Priyos <156951756+Priyosmita@users.noreply.github.com>
2024-12-15 14:29:20 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import React from "react";
-import GradientButton from "@/components/GradientButton";
-import Spotlight from "@/components/Spotlight/Spotlight";
-import Carousel3D from "@/components/Carousel3D";
+import React from 'react';
+import GradientButton from '@/components/GradientButton';
+import Carousel3D from '@/components/Carousel3D';
 
 const Page = () => {
+
   const items = [
     {
       image: "https://via.placeholder.com/300x200",
@@ -34,28 +34,20 @@ const Page = () => {
 
   return (
     <>
-      <div className="h-screen relative">
-        <Spotlight />
-        {/* Decorative Hand Image */}
-        <img
-          src="/assets/hand.png"
-          className="absolute bottom-0 left-0 w-full -top-8"
-          alt="Hand"
-        />
-        <div className="absolute inset-0 flex flex-col text-white">
-          {/* Title */}
-          <div className="text-9xl flex justify-center pt-36">AIkya</div>
-          <div className="text-xl mt-5 flex justify-center">
-            Where Ideas Create Impact
-          </div>
-          {/* Gradient Button */}
-          <div className="flex justify-center mt-16">
-            <GradientButton gradientColors={["#ff99a7", "#9bd4c3"]}>
-              <p className="text-center text-2xl">Dashboard</p>
+      {/* Parent container is relative */}
+      <div className='h-screen relative'>
+        {/* Child container is absolute */}
+        <img src='/assets/hand.png' className='absolute bottom-0 left-0 w-full -top-8' alt='Hand' />
+        <div className='absolute inset-0 flex flex-col text-white'>
+          <div className='text-9xl flex justify-center pt-36'>AIkya</div>
+          <div className='text-xl mt-5 flex justify-center'>Where Ideas Create Impact</div>
+          <div className='flex justify-center pt-96 mt-16'>
+            <GradientButton gradientColors={["#ff99a7", "#9bd4c3"]} className=''>
+              <p className='text-center text-2xl'>Dashboard</p>
             </GradientButton>
           </div>
           {/* Carousel Section */}
-          <div className="flex justify-center pt-36">
+          <div className="flex justify-center">
             <Carousel3D items={items} />
           </div>
         </div>
