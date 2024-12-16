32af78d306550b68e152ea04b31618649fbbf930
Rijurajx <iphonerijuraj@gmail.com>
2024-12-16 22:13:55 +0530

    styling footer

--- components/Footer.js
+++ components/Footer.js
@@ -24,7 +24,7 @@ const Footer = () => {
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-[#bdbdbd] hover:text-[#e4e3e3]">
                 Contact Us
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
@@ -47,7 +47,7 @@ const Footer = () => {
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-[#bdbdbd] hover:text-[#e4e3e3]">
                 Legal
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
